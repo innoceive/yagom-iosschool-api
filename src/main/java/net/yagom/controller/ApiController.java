feefdf86788464ca8a526ffe6f89179a72a243de
박정훈 <innoceive@gmail.com>
2016-11-13 12:08:10 +0900

    update 기능 추가

--- src/main/java/net/yagom/controller/ApiController.java
+++ src/main/java/net/yagom/controller/ApiController.java
@@ -2,7 +2,6 @@ package net.yagom.controller;
 
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
-import net.coobird.thumbnailator.resizers.configurations.ScalingMode;
 import net.yagom.component.AWSComponent;
 import net.yagom.domain.pojo.ImageItem;
 import net.yagom.domain.response.BaseResponse;
@@ -12,10 +11,15 @@ import net.yagom.service.ImageUploadService;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,18 +48,6 @@ public class ApiController {
         result.setList(imageItems);
         return result;
     }
-
-// TODO update 작업
-//    @RequestMapping(value = "/image", method = RequestMethod.UPDATE)
-//    public DataResponse<String> deleteImage(
-//            @RequestParam(name = "image_data") MultipartFile imageData,
-//            @RequestParam(name="image_id") Long id,
-//            @RequestParam(name="user_id") Long userId,
-//            @RequestParam(name = "title") String title
-//    ) {
-//
-//    }
-
 
     @RequestMapping(value = "/image", method = RequestMethod.DELETE)
     public DataResponse<String> deleteImage(
@@ -91,7 +83,8 @@ public class ApiController {
     public DataResponse<String> uploadThumbnail(
             @RequestParam(name = "image_data") MultipartFile imageData,
             @RequestParam(name = "title") String title,
-            @RequestParam(name = "user_id") Long userId
+            @RequestParam(name = "user_id") long userId,
+            @RequestParam(name="image_id", required = false) Long imageId
 //            @RequestHeader(value="X-ACCESS-USER") Long userId
     ) {
         final String mid = "IMAGE_UPLOAD";
@@ -109,8 +102,16 @@ public class ApiController {
             final String uuid = UUID.randomUUID().toString();
             final String imageUrl = awsComponent.upload(uuid + "." + ext, imageData.getInputStream());
             final String thumbnailImageUrl = awsComponent.upload(uuid + THUMBNAIL_SUFFIX + "." + ext, thumbnailInputStream);
-
             ImageItem imageItem = new ImageItem();
+            if(imageId != null) {
+                imageItem = imageUploadService.findById(imageId);
+                if(!imageItem.getUserId().equals(userId)) {
+                    response.setResult(BaseResponse.UNAUTHORIZED);
+                    response.setMessage("요청한 이미지에 대한 권한이 없습니다.");
+                    return response;
+                }
+            }
+
             imageItem.setImageUrl(imageUrl);
             imageItem.setThumbnailUrl(thumbnailImageUrl);
             imageItem.setTitle(title);
